a17274f4a0d81d14853b07d135ca5b190e655f74
Luc Claustres <luc.claustres@orange.fr>
2023-12-11 18:09:08 +0100

    fix: tests

--- .travis.test.sh
+++ .travis.test.sh
@@ -1,5 +1,19 @@
 echo "Running tests"
 
+check_code()
+{
+   if [ $1 -eq 1 ]; then
+	  echo "$2 has failed [error: $1]"
+	  exit 1
+  fi
+}
+
 # Run tests
 yarn install
+
+# Clone others direct dependencies we'd like to use for testing
+git clone https://github.com/kalisio/feathers-distributed && cd feathers-distributed && yarn install && yarn link && cd ..
+yarn link @kalisio/feathers-distributed
+
 yarn test
+check_code $? "Running tests"
